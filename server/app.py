9bd7a43ce20915889706259d9e0ff3217696e89c
Mary Monchari Gichana <marygichana95@gmail.com>
2025-01-27 21:24:29 +0300

    imported SQLALchemy, db, Migrate

--- server/app.py
+++ server/app.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from models import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
