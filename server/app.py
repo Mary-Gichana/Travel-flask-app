6351151446a65e12c2709937147d4efeada398ae
Mary Monchari Gichana <marygichana95@gmail.com>
2025-01-27 21:25:56 +0300

    imported Api, Resource,date modules

--- server/app.py
+++ server/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from models import db
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from date import date
 
 app = Flask(__name__)
 
