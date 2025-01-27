e237bdc795931cbaf726d0230a2884600b3d3d37
Mary Monchari Gichana <marygichana95@gmail.com>
2025-01-27 21:28:00 +0300

    Configure SQLite database and initialize app with SQLAlchemy, Migrate, and Api

--- server/app.py
+++ server/app.py
@@ -7,6 +7,14 @@ from date import date
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travel.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+api = Api(app)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
