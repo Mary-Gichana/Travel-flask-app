247cd4e8bc4da8389ac187089c1e341560cf0a8b
Mary Monchari Gichana <marygichana95@gmail.com>
2025-01-27 22:54:33 +0300

    Added GET method to User resource for retrieving all users

--- server/app.py
+++ server/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, make_response
 from flask_sqlalchemy import SQLAlchemy
-from models import db
+from models import db, User, Trip, Destination, Trip_Destination
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+
 from datetime import date
 
 
@@ -21,6 +22,13 @@ class Home(Resource):
         return {'Welcome to the travel app!'}
 
 api.add_resource(Home, '/')
+
+class User(Resource):
+    def get(self):
+        return make_response([user.to_dict() for user in User.query.all()], 200)
+    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
